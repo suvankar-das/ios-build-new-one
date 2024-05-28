795e6fe2d6b4fb45efa20c190fb9355b62fd77c0
Suvankar Das <hi.suvankar@gmail.com>
2024-05-28 10:44:59 +0530

    sss

--- ios/Runner/NativeViewFactory.swift
+++ ios/Runner/NativeViewFactory.swift
@@ -102,19 +102,6 @@ public class NativeView : NSObject, FlutterPlatformView,fullScreeenDelegate, IMA
         setUpAdsLoader()
         createNativeView(view: _view)
         startTimer()
-       
-       DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
-    // Make sure the object is still around after 2 minutes
-    guard let self = self else { return }
-    
-    // Make sure the URL is valid
-    guard let contentURL = URL(string: self.kTestAppContentUrl_MP4) else {
-        print("ERROR: please use a valid URL for the content URL")
-        return
-    }
-
-    // Start playing the video
-    self.playerView.play(for: contentURL)
 }
 
     }
@@ -186,7 +173,7 @@ public class NativeView : NSObject, FlutterPlatformView,fullScreeenDelegate, IMA
         controlView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 400)
 
         playerView.contentMode = .scaleAspectFill
-        playerView.play(for: contentURL)
+        //playerView.play(for: contentURL)
    
    
         controlView.delegate = self
@@ -210,6 +197,9 @@ public class NativeView : NSObject, FlutterPlatformView,fullScreeenDelegate, IMA
 
       // Set up our content playhead and contentComplete callback.
         contentPlayhead = IMAAVPlayerContentPlayhead(avPlayer: playerView.player!)
+        DispatchQueue.main.async {
+            self.playerView.play(for: contentURL)
+        }
     }
    
     @objc func touchHappen(_ sender: UITapGestureRecognizer) {
